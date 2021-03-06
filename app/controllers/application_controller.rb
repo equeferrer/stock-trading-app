ef0d70b572fbbc79e9709989faf877459216d6d2
JERICK CAMACHO <jerickcamacho42@gmail.com>
2021-03-06 10:28:50 +0800

    Set up Single Table Inheritance using Devise for Admin, Buyer, Broker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    # app/controllers/application_controller.rb
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end 
+
+    devise_group :user, contains: [:admin, :buyer, :broker]
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # root "home#index"
+  get 'home/index'
+  #devise_for :users
+
+  devise_for :admins
+  devise_for :buyers
+  devise_for :brokers
+
+  root "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
